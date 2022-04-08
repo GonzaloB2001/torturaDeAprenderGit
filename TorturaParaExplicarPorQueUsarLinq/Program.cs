799f1c1279158681bf47e91217a417c846ac01f9
GonzaloB2001 <you@example.com>
2022-04-07 21:16:12 -0300

    primer commit

---
+++ TorturaParaExplicarPorQueUsarLinq/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TorturaParaExplicarPorQueUsarLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numerosList = new List<int>() { 3,6,2,9,7,4,1,7,8 };
+            numerosList.Sort((num1, num2) => num1 - num2);
+
+
+            foreach (var value in numerosList)
+                Console.WriteLine(value);
+        }
+    }
+}
+
