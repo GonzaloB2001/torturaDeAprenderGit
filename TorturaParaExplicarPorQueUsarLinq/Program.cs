d6fb45c19fd51c05e57795c53cc21d34854ef09d
GonzaloB2001 <you@example.com>
2022-04-07 22:16:30 -0300

    TorturaDeRequisitos

--- TorturaParaExplicarPorQueUsarLinq/Program.cs
+++ TorturaParaExplicarPorQueUsarLinq/Program.cs
@@ -10,7 +10,7 @@ namespace TorturaParaExplicarPorQueUsarLinq
         {
             List<int> numerosList = new List<int>() { 3,6,2,9,7,4,1,7,8 };
             numerosList.Sort((num1, num2) => num1 - num2);
-
+            //
 
             foreach (var value in numerosList)
                 Console.WriteLine(value);
